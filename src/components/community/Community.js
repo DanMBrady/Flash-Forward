5eb8d88560110b337742bf88d6f66a73f034eeeb
Dan Brady <dannymb360@gmail.com>
2023-04-17 16:32:31 -0400

    add comic

--- src/components/community/Community.js
+++ src/components/community/Community.js
@@ -1,6 +1,21 @@
 import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 export const Community=()=>{
-    const[comics,setComics]=useState([])
+    const [comics,setComics]=useState([])
+    const [reviews,setReviews]=useState([])
+
+    const localHoneyUser = localStorage.getItem("honey_user")
+    const honeyUserObject = JSON.parse(localHoneyUser)
+
+    const navigate= useNavigate()
+
+    const getAllReviews= ()=>{
+        fetch("http://localhost:8088/reviews")
+       .then(response => response.json())
+       .then((ticketArray) => {
+        setReviews(ticketArray)
+       })
+    }
 
     useEffect(
         ()=>{
@@ -13,16 +28,64 @@ export const Community=()=>{
         },
         []
     )
+
+    useEffect(
+        ()=>{
+             fetch(`http://localhost:8088/reviews`)
+            .then(response => response.json())
+            .then((reviewArray)=>{
+                setReviews(reviewArray)
+            })
+            
+        },
+        []
+    )
+
+    const handleSaveButtonClick = (event,comicId) => {
+        event.preventDefault()
+        
+    
+       
+        const ToSendToAPIReview = {
+         readStatId:1,
+         review:"",
+         userId:honeyUserObject.id,
+         comicId:comicId,
+         isFavorite:false
+        }
+    
+      
+    
+        
+        return fetch("http://localhost:8088/reviews", {
+            method:"POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body:JSON.stringify(ToSendToAPIReview)
+        })
+        .then(response => response.json())
+        .then(getAllReviews)
+    }
     return <article>
        <h1>Community Libary</h1> 
        <div className="comicContainer">
         {
             comics.map(comic=>{
-                return <article key ={`comic--${comic.id}`} className="comic">
+                const comicReviews= reviews.filter(review=>review.comicId===comic.id)
+                const userReview = comicReviews.filter(comic=> comic.userId === honeyUserObject.id)
+                const comicId=comic.id
+                return <article key ={`comic--${comic.id}`} className="comicC">
+                    <div className="comicTop">
                     <section><img className="image"src={comic.photo}></img></section>
                     <section>{comic.title}</section>
                     <section>Author: {comic.author}</section>
                     <section>Era: {comic?.era?.era}</section>
+                    </div>
+                    <section>{ (userReview.length === 0) ?
+                        <button className="buttonR" onClick ={(clickEvent)=> handleSaveButtonClick(clickEvent,comicId)}>Add Comic</button> : ""
+                        }</section>
+                       
                 </article>
             })
         }
