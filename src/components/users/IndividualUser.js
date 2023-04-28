1fa0e25e06cf25572b6371481c46b7a315f9cb21
Dan Brady <dannymb360@gmail.com>
2023-04-28 11:42:23 -0400

    user admins

--- src/components/users/IndividualUser.js
+++ src/components/users/IndividualUser.js
@@ -5,9 +5,12 @@ import { useParams } from "react-router-dom"
 export const IndividualUser =()=>{
     const {userId} = useParams()
     const [reviews,setReviews]=useState([])
+    const [comics,setComics]=useState([])
      const [user,setUser]=useState({
-    fullName:"joey"
+    fullName:"joey",
+    isAdmin: false
 })
+const navigate =useNavigate()
 useEffect(
     ()=>{
         fetch(`http://localhost:8088/users?id=${userId}`)
@@ -30,6 +33,35 @@ useEffect(
     },
     []
 )
+useEffect(
+    ()=>{
+         fetch(`http://localhost:8088/comics?userId=${userId}`)
+        .then(response => response.json())
+        .then((comicArray)=>{
+            setComics(comicArray)
+        })
+        
+    },
+    []
+)
+
+const handleSaveButtonClick = (event) => {
+    event.preventDefault()
+    
+    return fetch(`http://localhost:8088/users/${userId}`, {
+            method:"PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body:JSON.stringify(user)
+    })
+    .then(response => response.json())
+    .then(() =>{
+        navigate("/users")
+    }) 
+}
+
+
     return<article className="addFifty">
         <h1>User Profile</h1>
         <div className="profileContainer">
@@ -37,10 +69,79 @@ useEffect(
             <section className="spaceProfile">Full Name: {user.fullName}</section>
             <section className="spaceProfile">Username: {user.userName}</section>
             <section className="spaceProfile">Email: {user.email}</section>
-            <section className="spaceProfile">Number of Comics: {reviews.length}</section>
-            <section className="spaceProfile">Admin:</section>
-            <button className="spaceProfile"> Save Profile</button>
-            <button className="spaceProfile"> Delete Profile</button>
+            <section className="spaceProfile">Total Number of Comics: {reviews.length}</section>
+            <section className="spaceProfile">Number of Comics Uploaded: {comics.length}</section>
+            <section className="spaceProfile">Admin: {
+
+                (user.isAdmin === true) ? <div className="userSpace">
+                Yes
+                 <input
+                     required 
+                     type="radio"
+                     name="era"
+                     value={user.isAdmin}
+                     className="eraForm profileRadio"
+                     defaultChecked
+                     onChange={
+                         (evt) => {
+                            const copy ={...user}
+                            copy.isAdmin =true
+                            setUser(copy)
+                         }
+                     } /> 
+                 
+                      No
+                 <input
+                     required 
+                     type="radio"
+                     name="era"
+                     value={user.isAdmin}
+                     className="eraForm profileRadio"
+                     onChange={
+                         (evt) => {
+                            const copy ={...user}
+                            copy.isAdmin =false
+                            setUser(copy)
+                         }
+                     } />
+</div>  : "" } {
+
+                        (user.isAdmin === false) ? <div className="userSpace">
+                        Yes
+                        <input
+                            required 
+                            type="radio"
+                            name="era"
+                            value={user.isAdmin}
+                            className="eraForm profileRadio"
+                            onChange={
+                                (evt) => {
+                                    const copy ={...user}
+                                    copy.isAdmin =true
+                                    setUser(copy)
+                                }
+                            } /> 
+                        
+                            No
+                        <input
+                            required 
+                            type="radio"
+                            name="era"
+                            value={user.isAdmin}
+                            className="eraForm profileRadio"
+                            defaultChecked
+                            onChange={
+                                (evt) => {
+                                    const copy ={...user}
+                                    copy.isAdmin =false
+                                    setUser(copy)
+                                }
+                            } />
+                        </div>  : "" }
+                                                        
+                                                        
+                                                        </section>
+            <button className="spaceProfile profileButton" onClick ={(clickEvent)=> handleSaveButtonClick(clickEvent)}> Save Profile</button>
         </article>
         </div>
     </article>
